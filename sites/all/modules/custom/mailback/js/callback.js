c1d58f3992039a1b0eba0e49d0fb9501f8a00980
gruzchik17 <manjesus17@gmail.com>
2015-05-07 12:13:35 +0700

    Добавлен модуль  0,1#13

---
+++ sites/all/modules/custom/mailback/js/callback.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(function () {
+        jQuery(document).ready(function () {
+            
+            $("#edit-mail").mask("[a-z0-9_.-]*@[a-z0-9_.-]*");
+            
+         });
+    });
+})(jQuery);
